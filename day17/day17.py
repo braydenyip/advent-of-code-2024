990a350e5f372d2f2f9f156fdb7869f91a512894
Brayden Yip <yipb@uoguelph.ca>
2024-12-24 01:21:55 -0500

    day 17b

--- day17/day17.py
+++ day17/day17.py
@@ -63,47 +63,43 @@ def run_program(regs, program):
         jumped = False
     return output[:-1]
 
-def run_program_until(regs, program):
-    output = []
-    ip, opcode, output_ptr = 0, 0, 0
-    jumped = False
-    while (ip < len(program)-1):
-        
-        opcode = program[ip]
-        operand = get_operand_value(regs, program[ip+1])
-        # regs_tuple = (regs[0], regs[1], regs[2], ip)
-        # if regs_tuple in program_states[opcode]:
-        #     return False
-        # else:
-        #     program_states[opcode].add(regs_tuple)
+def run_fast_until(a, program):
+    # Note that since the regs[0] is modified only after regs[1] is extracted from it,
+    # we can just divide it by 8 every loop...
+    # Working backwards:
+    # the last operation before output is regs[1] = regs[1] xor regs[2]
+    # regs[2] = regs[0] / 2^regs[1] after the first step
+    # i.e., regs[2] = regs[0] / 2^(regs[0]%8 xor 1) = regs[0] >> (regs[0]%8 xor 1)
+    # regs[1] = (regs[0]) % 8 xor 1 xor 4
+    # This means on every pass regs[1] = (regs[0]) % 8 xor 1 xor 4
+    # Thus, regs[1] = [(regs[0]) % 8 xor 1 xor 4] xor [regs[0] >> (regs[0]%8 xor 1)]
+    res = []
+    index = 0
+    while a > 0:
+        m8 = a%8
+        print_num = (m8 ^ 1 ^ 4) ^ (a >> (m8 ^ 1))
+        if print_num == program[index]:
+            res.append(print_num)
+            index += 1
+        else:
+            break
+        a = a >> 3
+    return res
 
-        # print(f"perform opcode {opcode} with regs {regs} and combo operand {operand}")
-        if opcode == 0:
-            # ad
-            regs[0] = regs[0] >> operand
-        elif opcode == 1:
-            regs[1] = regs[1] ^ program[ip+1]
-        elif opcode == 2:
-            regs[1] = operand % 8
-        elif opcode == 3 and regs[0] != 0:
-            ip = program[ip+1]
-            jumped = True
-        elif opcode == 4:
-            regs[1] = regs[1] ^ regs[2]
-        elif opcode == 5:
-            out_value = operand%8
-            if output_ptr > (len(program) - 1) or out_value != program[output_ptr]:
-                return False
-            else:
-                output_ptr += 1
-                output.append(out_value)
-        elif opcode == 6 or opcode == 7:
-            regs[opcode-5] = regs[0] >> operand
-        if not jumped:
-            ip += 2
-        jumped = False
-    return output == program
 
+def search_space_helper(i, current_a, program):
+    if i == -1:
+        print(f"final a={current_a}")
+        return -1
+    current_a = current_a << 3    
+    for j in range(8):
+        new = current_a + j
+        res = run_program([new, 0, 0], program).split(",")[0]
+        if int(res) == program[i]:
+            dfs_res = search_space_helper(i-1, new, program)
+            if dfs_res >= 0:
+                return current_a
+    return -1
 if __name__ == "__main__":
     regs, program = import_input("day17.txt")
     program_literal = ""
@@ -111,14 +107,11 @@ if __name__ == "__main__":
         program_literal += f"{o},"
     program_literal = program_literal[:-1]
     print(f"Program: {program_literal}")
-    for i in range(8):
-        program_states[i] = set()
+
     output = run_program(regs, program)
     print(f"Part A: {output}")
-    a = 0
-    regs = [0,0,0]
-    while (a < 99000000) and not run_program_until(regs, program):
-        regs[0], regs[1], regs[2] = a, 0, 0
-        a += 1
+
+    a = search_space_helper(len(program)-1, 0, program)
+    print(a)
     print("END")
             
